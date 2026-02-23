3973db9b3c1aed5deba2ecb239204b7e7bfe8b84
garanceledoigt <123960829+garance351@users.noreply.github.com>
2026-02-23 15:50:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def MaPremiereAPI():
 @app.get("/paris")
 def api_paris():
     
-    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m&forecast_days=7"
+    
     response = requests.get(url)
     data = response.json()
 
@@ -23,13 +24,13 @@ def api_paris():
     temps = data.get("hourly", {}).get("temperature_2m", [])
 
     n = min(len(times), len(temps))
+
     result = [
         {"datetime": times[i], "temperature_c": temps[i]}
         for i in range(n)
     ]
 
     return jsonify(result)
-
 
 
 # Ne rien mettre après ce commentaire
